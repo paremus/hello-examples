37c9b2b1ac430227f0d92bf336be6cf5a59c079a
Tim Ward <timothyjward@apache.org>
2016-05-18 17:09:08 +0100

    Add configurable language support to the greeting service

--- helloworld/helloworld-ds/src/main/java/com/example/hello/impl/GreetingImpl.java
+++ helloworld/helloworld-ds/src/main/java/com/example/hello/impl/GreetingImpl.java
@@ -1,13 +1,55 @@
 package com.example.hello.impl;
 
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.example.hello.Greeting;
 
-@Component
+@Designate(ocd = GreetingImpl.Config.class)
+@Component(configurationPid = "com.example.hello.ds")
 public class GreetingImpl implements Greeting {
+
+    private static final Logger logger = LoggerFactory.getLogger(GreetingImpl.class);
+
+    @ObjectClassDefinition(id = "com.example.hello.ds", name = "Hello Message Service configuration")
+    @interface Config {
+        @AttributeDefinition(required = false)
+        String language() default "en";
+    }
+
+    private static final String RESOURCE_BASE = "com.example.hello.impl.greetings";
+
+    private ResourceBundle resources;
+
+    @Activate
+    public void activate(Config config) {
+        logger.info("Setting Hello Greeting Service language to {}", config.language());
+        setResourceBundle(config);
+    }
+
+    @Modified
+    public void modified(Config config) {
+        logger.info("Changing Hello Greeting Service language to {}", config.language());
+        setResourceBundle(config);
+    }
+
+    private void setResourceBundle(Config config) {
+        Locale locale = new Locale(config.language());
+        resources = ResourceBundle.getBundle(RESOURCE_BASE, locale);
+    }
+
     @Override
     public String sayHello(String name) {
-        return "Hello, " + name + "!";
+        return MessageFormat.format(resources.getString("greeting"), name);
     }
 }
--- helloworld/helloworld-ds/src/test/java/com/example/hello/impl/GreetingImplTest.java
+++ helloworld/helloworld-ds/src/test/java/com/example/hello/impl/GreetingImplTest.java
@@ -1,13 +1,40 @@
 package com.example.hello.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.example.hello.impl.GreetingImpl.Config;
+
+@RunWith(MockitoJUnitRunner.class)
 public class GreetingImplTest {
+    
+    @Mock
+    Config config;
+    
     @Test
     public void testSaysHello() {
-        String result = new GreetingImpl().sayHello("NewYear");
+        when(config.language()).thenReturn("en");
+        GreetingImpl greetingImpl = new GreetingImpl();
+
+        greetingImpl.activate(config);
+        
+        String result = greetingImpl.sayHello("NewYear");
         assertEquals("Hello, NewYear!", result);
+    }
+
+    @Test
+    public void testSaysHelloFrench() {
+        when(config.language()).thenReturn("fr");
+        GreetingImpl greetingImpl = new GreetingImpl();
+        
+        greetingImpl.activate(config);
+        
+        String result = greetingImpl.sayHello("NewYear");
+        assertEquals("Bonjour NewYear.", result);
     }
 }
